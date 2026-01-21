27a4ff95a0e36a2d71f2b11529256fe6be90f5e5
Fabian144 <fabianyousif24@gmail.com>
2026-01-21 01:50:03 +0100

    Add code

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,77 @@
-import { useState } from 'react';
-import './App.css';
+type WikiApiResponse = [string, string[], string[], string[]];
+type Term = { term: string; time: Date };
+type Result = { title: string; url: string };
 
-function App() {
-  return <></>;
+export default function App() {
+  let response: WikiApiResponse;
+  let searchTerm: Term = { term: '', time: new Date() };
+  let a = new Date().toLocaleString;
+  console.log(a);
+
+  async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+  }
+
+  return (
+    <div className="flex flex-col justify-center items-center min-h-dvh bg-gray-100 p-4">
+      <div className="flex flex-col gap-8 w-full max-w-2xl">
+        <h1 className="text-3xl font-semibold text-gray-700">Wiki fetching</h1>
+
+        <form
+          onSubmit={handleSubmit}
+          className="p-8 rounded-2xl shadow-md flex flex-col border border-neutral-200"
+        >
+          <label htmlFor="search" className="text-xl font-medium text-gray-600 mb-2">
+            Search: {searchTerm.term}
+          </label>
+
+          <div className="flex">
+            <input
+              id="search"
+              name="Term"
+              type="text"
+              className="grow font-light rounded-l-lg border-gray-300 border px-4 py-2"
+            />
+
+            <button
+              type="submit"
+              className="bg-teal-500 text-white px-4 py-2 rounded-r-lg hover:bg-teal-600 transition-colors cursor-pointer"
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+
+        <div className="grid grid-cols-2 gap-8 border p-8 border-neutral-200 rounded-2xl shadow-md min-h-96">
+          <div>
+            <h2 className="text-2xl mb-4">Results</h2>
+
+            <ul>
+              <li>
+                <a
+                  href="#"
+                  className="underline text-blue-500 hover:text-blue-700"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  HelloWorld
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          <div>
+            <h2 className="text-2xl mb-4">Last 5 terms</h2>
+
+            <ul>
+              <li className="gap-2 flex">
+                <span className="font-medium">HelloWorld - </span>
+                <span className="font-light">9/4/2025, 12:59:23 PM</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
-
-export default App;
